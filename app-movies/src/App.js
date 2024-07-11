2f37d7a4223fb7caf278a74d8c61a9ae9d225e40
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-07-11 14:19:51 -0300

    aula5

--- app-movies/src/App.js
+++ app-movies/src/App.js
@@ -3,6 +3,8 @@ import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header/Index";
+import Card from './components/Card/Index';
+import videos from './Json/db.json';
 
 function App() {
   return (
@@ -10,8 +12,20 @@ function App() {
       <Header />
       <Banner image="imgg" />
       <Container>
-        <h2>Testando</h2>
-        <p>Safosadjfóasjfgóasgsdhsfghg</p>
+      <h2>Geografia</h2>
+       <section className='cards'>
+          
+
+       {
+
+          videos.map(video=>{
+            return <Card id={video.id} key={video.id}></Card>
+          })
+
+       }
+
+
+       </section>
       </Container>
       <Footer />
     </>
